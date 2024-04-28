0d8ad61d13307e97e6210815e5ec0a37917965d8
herin7 <herinsoni3737@gmail.com>
2024-04-28 21:41:29 +0530

    Basic  Functionalities added

--- bb4main/urls.py
+++ bb4main/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    
+     path("add/",views.add_food_item,name='add_food_item'),
+     path("view/",views.view_food_items,name='view_food_items'),
+     path('accounts/', include('allauth.urls')),
      path("logout",views.logout_view),
      path("",views.home)
 ]
+     
--- bb4main/views.py
+++ bb4main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import FoodItemForm
 from django.contrib.auth import logout
-# Create your views here.
-# It iss Request Handler
+from django.contrib.auth.decorators import login_required
+
+from .models import FoodItem
 
 def home(request):
     return render(request, 'home.html')
@@ -12,3 +14,24 @@ def home(request):
 def logout_view(request):
     logout(request)
     return redirect("/")
+
+
+
+@login_required
+def add_food_item(request):
+    if request.method == 'POST':
+        form = FoodItemForm(request.POST)
+        if form.is_valid():
+            food_item = form.save(commit=False)
+            food_item.user = request.user  
+            food_item.save()
+            return redirect('view_food_items')
+    else:
+        form = FoodItemForm()
+    return render(request, 'add_food_item.html', {'form': form})
+
+
+@login_required
+def view_food_items(request):
+    food_items = FoodItem.objects.all()
+    return render(request,'view_food_items.html', {'food_items' : food_items})
