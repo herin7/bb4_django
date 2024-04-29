fce38866dbb83423dcecf7f5bc0af53686714a76
herin7 <herinsoni3737@gmail.com>
2024-04-29 17:30:22 +0530

    Pages ADDED

--- bb4main/views.py
+++ bb4main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .forms import FoodItemForm
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-
+import datetime
 from .models import FoodItem
 
 def home(request):
@@ -18,20 +18,33 @@ def logout_view(request):
 
 
 @login_required
-def add_food_item(request):
-    if request.method == 'POST':
-        form = FoodItemForm(request.POST)
-        if form.is_valid():
-            food_item = form.save(commit=False)
-            food_item.user = request.user  
-            food_item.save()
-            return redirect('view_food_items')
-    else:
-        form = FoodItemForm()
-    return render(request, 'add_food_item.html', {'form': form})
-
-
-@login_required
 def view_food_items(request):
     food_items = FoodItem.objects.all()
     return render(request,'view_food_items.html', {'food_items' : food_items})
+
+
+
+
+
+
+@login_required
+def save_fooditem(request):
+    if request.method == 'POST':
+        product_name = request.POST.get('product_name')
+        product_type = request.POST.get('product_type')
+        quantity = request.POST.get('quantity')
+        expiry_date = request.POST.get('expiry_date')
+        
+        fooditem = FoodItem.objects.create(user=request.user, product_name=product_name, product_type=product_type, quantity=quantity, expiry_date=expiry_date)
+        
+        
+        return redirect('view_food_items')  # Redirect to a success page
+    return render(request, 'add_food_item.html')
+
+
+def fillter(request):
+    now=datetime.datetime.now()
+    food_items = FoodItem.objects.expiry_date
+    return render(request, "view_food_items.html",{
+        "current": now.year >  food_items.year
+    })
