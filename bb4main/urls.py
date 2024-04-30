21eab07418960ec3e806cd170ec6761d3af804cd
herin7 <herinsoni3737@gmail.com>
2024-04-30 12:38:26 +0530

    PRIVATE KEYS REMOVED

--- bb4main/urls.py
+++ bb4main/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
      path("add/",views.save_fooditem,name='save_fooditem'),
      path("view/",views.view_food_items,name='view_food_items'),
      path('accounts/', include('allauth.urls')),
-     path("logout",views.logout_view),
+      path('logout/', views.logout_view, name='logout'),
      path("",views.home),
      path("chat/",views.chatbot,name='chatbot'),
    
--- bb4main/views.py
+++ bb4main/views.py
@@ -7,22 +7,29 @@ from .models import FoodItem
 import os
 import google.generativeai as genai
 from google.generativeai import GenerativeModel
+import configparser
 
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\Herin\Desktop\gitclones\bb4\bb4_django\gen-lang-client-0308490624-498c561275d3.json"
+# Read API key from the configuration file
+config = configparser.ConfigParser()
+config.read('config.ini')
+api_key = config['GENAI']['API_KEY']
+credentials_file_path = config['GENAI']['CREDENTIALS_FILE_PATH']
 
-genai.configure(api_key=os.getenv("AIzaSyCyoXP2XL77NxhXgXi4dVv6n9nmKniZpb0"))
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_file_path
+
+genai.configure(api_key=os.getenv("API_KEY"))
 generation_config = {"temperature": 0.9, "top_p": 1, "top_k": 1, "max_output_tokens": 2048}
 model = genai.GenerativeModel("gemini-pro", generation_config=generation_config)
 
 
 def home(request):
-    return render(request, 'home.html')
-
+    return redirect('/view')
 
 def logout_view(request):
     logout(request)
-    return redirect("/")
+    return render(request,'home.html')
 
 
 @login_required
@@ -30,7 +37,7 @@ def view_food_items(request):
     food_items = FoodItem.objects.filter(user=request.user)
     expired_food_items = [item for item in food_items if item.expiry_date < datetime.date.today()]
     return render(request, 'view_food_items.html', {'food_items': food_items, 'expired_food_items': expired_food_items})
-
+ 
 
 @login_required
 def save_fooditem(request):
@@ -42,26 +49,19 @@ def save_fooditem(request):
 
         fooditem = FoodItem.objects.create(user=request.user, product_name=product_name, product_type=product_type, quantity=quantity, expiry_date=expiry_date)
 
-        return redirect('view_food_items')  # Redirect to a success page
+        return redirect('/view')  # Redirect to a success page
     return render(request, 'add_food_item.html')
 
 
 @login_required
 def chatbot(request):
     user_input = request.POST.get('user-input', '')
-
     food_items = FoodItem.objects.filter(user=request.user)
-
     food_item_details = "\n".join([f"{item.product_name} - Quantity: {item.quantity}, Expiry Date: {item.expiry_date}" for item in food_items])
-
     merged_input = user_input + "\n\nList of your food items:\n" + food_item_details
-
     default_input = "Answer user's question only after analyzing the food items."
     merged_input += "\n\n" + default_input
-
     response = model.generate_content([merged_input])
-
     chatbot_response = response.text
-
     return render(request, 'chatbot.html', {'response': chatbot_response})
 
